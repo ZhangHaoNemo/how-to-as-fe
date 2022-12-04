3dc8bd2993266715e9af31d498ee483779459885
zhanghao <zhanghao@shopee.com>
2022-12-05 00:02:53 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const Child = ({ onButtonClick }) => {
+  console.log('Child Render')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button onClick={onButtonClick}>button</button>
+  )
 }
+
+function App() {
+  console.log('App render')
+  const [number, setNumber] = useState(0)
+
+  const addClick = () => {
+    setNumber(1)
+  }
+  useEffect(() => {
+    console.log('effect' + number)
+  }, [number])
+
+  return (
+    <div>
+      <Child onButtonClick={addClick} />
+    </div>
+  )
+}
+
 
 export default App;
